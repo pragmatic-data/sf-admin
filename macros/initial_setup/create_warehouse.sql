6e65ed8dc96d1d42585b4551143ce5cdc8dcb1c1
rz70rz70@gmail.com <rz70rz70@gmail.com>
2024-08-18 17:27:07 +0000

    Added test for create_warehouse

--- macros/initial_setup/create_warehouse.sql
+++ macros/initial_setup/create_warehouse.sql
@@ -1,28 +1,35 @@
 
-{% macro create_warehouse(prj_name, sysadmin_root_role , env_name = none) -%}
+{% macro create_warehouse(prj_name 
+                        , env_name = none
+                        , owner_role = none
+                        , creator_role = none
+                          ) -%}
+    {% set creator_role = creator_role or var('creator_role', 'SYSADMIN') %}
+    {% set owner_role = owner_role or var('owner_role', 'SYSADMIN') %}
+    {% set wh_name = sf_project_admin.get_warehouse_name(prj_name, env_name) %}
 
-  USE ROLE SYSADMIN;
+    {%- do log("*+  Creating warehouse " ~ wh_name ~ " for project " ~ prj_name ~ ", environment = " ~ env_name, info=True) -%}
 
-  {% set wh_name = sf_project_admin.get_warehouse_name(prj_name, env_name) %}
+    USE ROLE {{creator_role}};
 
-  {%- do log("*+  Creating warehouse " ~ wh_name ~ " for project " ~ prj_name ~ ", environment = " ~ env_name, info=True) -%}
+    /** 1 Create Warehouse */
+    CREATE OR REPLACE WAREHOUSE {{wh_name}} WITH
+        WAREHOUSE_SIZE = XSMALL 
+        SCALING_POLICY = ECONOMY
+        AUTO_SUSPEND = 120                -- 120 seconds = 2 minutes
+        AUTO_RESUME = TRUE
+        INITIALLY_SUSPENDED = TRUE
+        COMMENT = 'Warehouse for {{prj_name}} project{{ ' and '~env_name~' environment.' if env_name else '.'}}'
+    ;
 
-  /** 1 Create Warehouse */
-  CREATE OR REPLACE WAREHOUSE {{wh_name}} WITH
-    WAREHOUSE_SIZE = XSMALL 
-    SCALING_POLICY = ECONOMY
-    AUTO_SUSPEND = 120                -- 120 seconds = 2 minutes
-    AUTO_RESUME = TRUE
-    INITIALLY_SUSPENDED = TRUE
-    COMMENT = 'Warehouse for {{prj_name}} project.'
-  ;
-  {%- do log("**  Created warehouse " ~ wh_name ~ " for project " ~ prj_name ~ ", environment = " ~ env_name, info=True) %}
+    {%- do log("**  Created warehouse " ~ wh_name ~ " for project " ~ prj_name ~ ", environment = " ~ env_name, info=True) %}
 
-  /** 2 Assign Warehouse ownership to desired SysAdmin */
-  GRANT OWNERSHIP ON WAREHOUSE {{wh_name}} TO ROLE {{sysadmin_root_role}};
-  {%- do log("**  Assigned ownerhip of warehouse " ~ wh_name ~ " to role " ~ sysadmin_root_role, info=True) %}
+    /** 2 Assign Warehouse ownership to desired SysAdmin */
+    GRANT OWNERSHIP ON WAREHOUSE {{wh_name}} TO ROLE {{owner_role}};
 
-  {%- do log("*-  DONE with warehouse creation for project " ~ prj_name ~ ", environment = " ~ env_name, info=True) %}
+    {%- do log("**  Assigned ownerhip of warehouse " ~ wh_name ~ " to role " ~ owner_role, info=True) %}
+    {%- do log("*-  DONE with warehouse creation for project " ~ prj_name ~ ", environment = " ~ env_name, info=True) %}
+
 {%- endmacro %}
 
 
